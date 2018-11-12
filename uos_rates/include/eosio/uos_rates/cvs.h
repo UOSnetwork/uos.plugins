b3e0e99636268f49c234ed9d02984dc1311fd3a6
d.postnikov <silverstringer@gmail.com>
2018-11-12 18:24:08 +0300

    add compression for log file

--- uos_rates/include/eosio/uos_rates/cvs.h
+++ uos_rates/include/eosio/uos_rates/cvs.h
@@ -9,6 +9,12 @@
 #include <iterator>
 #include <fstream>
 
+#include <boost/iostreams/filtering_stream.hpp>
+#include <boost/iostreams/filter/zlib.hpp>
+#include <boost/iostreams/filter/gzip.hpp>
+#include <boost/iostreams/copy.hpp>
+#include <boost/filesystem.hpp>
+
 /**
  * @brief
  *  class for logger(helper for write result )
@@ -17,17 +23,22 @@
 std::string c_fail = "\033[1;31;40m";
 std::string c_clear = "\033[1;0m";
 
+using node_type = singularity::node_type;
+namespace bfs = boost::filesystem;
+namespace bio = boost::iostreams;
 
 class CSVWriter
 {
-    std::string fileName;
-    std::string delimeter;
+    using string = std::string;
+
+    string fileName;
+    string delimeter;
     int linesCount;
     bool is_apart, is_write;
-    const std::string path{"/home/calc_log/"};
+    const string path{std::string(getenv("HOME")) + "/uos/calc_log/"};
 
 public:
-    CSVWriter(std::string filename, std::string delm = ";", int lc  = 1, bool write = false ) :
+    CSVWriter(string filename, string delm = ";", int lc  = 1, bool write = false ) :
             fileName(filename), delimeter(delm), linesCount(lc), is_write(write)
     {}
     CSVWriter(const CSVWriter & vs)
@@ -41,14 +52,22 @@ public:
     template<typename T>
     void addDatainRow(T first, T last);
 
-    void inline settings(bool is_apart, bool is_write){this->is_apart = is_apart; this->is_write = is_write;}
-
-    void inline setFilename(std::string filename){
+    void inline setFilename(string filename){
         if(!is_write)
             return;
-        if ( !boost::filesystem::exists( path ) )
-            boost::filesystem::create_directories(path);
-        this->fileName = path+filename;
+        if ( !bfs::exists( path ) )
+            bfs::create_directories(path);
+        this->fileName = path + filename;
+    }
+
+    string inline getFilename() { return fileName;};
+
+    void inline settings(bool is_apart, bool is_write, string filename = "" )
+    {
+        this->is_apart = is_apart;
+        this->is_write = is_write;
+        if(!filename.empty())
+        setFilename(filename);
     }
 };
 
@@ -85,13 +104,82 @@ void CSVWriter::addDatainRow(T first, T last)
     file.close();
 }
 
-std::string to_string_from_enum(singularity::node_type e) {
-    switch (e) {
-        case singularity::node_type::ACCOUNT: return "ACCOUNT";
-        case singularity::node_type::CONTENT: return "CONTENT";
-        case singularity::node_type::ORGANIZATION: return "ORGANIZATION";
+
+
+std::string to_string_from_enum(node_type type) {
+    switch (type) {
+        case node_type::ACCOUNT: return "ACCOUNT";
+        case node_type::CONTENT: return "CONTENT";
+        case node_type::ORGANIZATION: return "ORGANIZATION";
     }
     return {};
+}
+
+inline uint64_t convert(std::string const& value) {
+    uint64_t result = 0;
+    char const* p = value.c_str();
+    char const* q = p + value.size();
+    while (p < q) {
+        result *= 10;
+        result += *(p++) - '0';
+    }
+    return result;
+}
+
+bool compressFile(std::string filename)
+{
+    bfs::path pathObj(filename);
+    if (pathObj.has_extension()) {
+        if (pathObj.extension().string() == "csv" /*&& !bfs::exists(bfs::change_extension(pathObj, "gzip").string())*/) {
+            std::ifstream inStream(filename, std::ios_base::in);
+            std::ofstream outStream(bfs::change_extension(pathObj, "gzip").string(), std::ios_base::out | std::ios_base::binary);
+            bio::filtering_streambuf<bio::input> in;
+            in.push(bio::gzip_compressor());
+            in.push(inStream);
+            bio::copy(in, outStream);
+            inStream.close();
+            outStream.close();
+            return true;
+        }
+    }
+}
+
+bool decompressFile(std::string filename)
+{
+    bfs::path pathObj(filename);
+    if (pathObj.has_extension()) {
+        if (pathObj.extension().string() == "gzip") {
+            std::ifstream inStream(filename, std::ios_base::in);
+            std::ofstream outStream(bfs::change_extension(filename, "csv").string(), std::ios_base::out | std::ios_base::binary);
+            bio::filtering_streambuf<bio::input> in;
+            in.push(bio::gzip_decompressor());
+            in.push(inStream);
+            bio::copy(in, outStream);
+            return true;
+        }
+    }
+
+}
+
+std::vector<bfs::directory_entry> listFileinDir (std::string path = std::string(getenv("HOME")) + "/uos/calc_log/")
+{
+    std::vector<bfs::directory_entry> v; // To save the file names in a vector.
+    if(bfs::is_directory(path))
+    {
+        copy(bfs::directory_iterator(path), bfs::directory_iterator(), back_inserter(v));
+    }
+
+    return v;
+}
+
+void compressed()
+{
+    std::vector<bfs::directory_entry> v= listFileinDir();
+    for ( std::vector<bfs::directory_entry>::const_iterator it = v.begin(); it != v.end();  ++ it )
+    {
+        compressFile((*it).path().string());
+    }
+
 }
 
 
