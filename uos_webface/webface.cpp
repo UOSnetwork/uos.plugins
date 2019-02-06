f75f2b9ca0304f8998ce320be81d3d8ad65bd661
Vladimir <37449014+vldmrweider@users.noreply.github.com>
2019-02-06 11:30:06 +0300

    Update webface.cpp

--- uos_webface/webface.cpp
+++ uos_webface/webface.cpp
@@ -407,6 +407,17 @@ public:
     void
     on_socketerr(boost::system::error_code ec)
     {
+       auto const bad_request =
+       [&req](boost::beast::string_view why)
+            {
+                http::response<http::string_body> res{http::status::bad_request, req.version()};
+                res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
+                res.set(http::field::content_type, "text/html");
+                res.keep_alive(req.keep_alive());
+                res.body() = why.to_string();
+                res.prepare_payload();
+                return res;
+            };
         if(ec)
         {
             fail(ec, "socket accept");
